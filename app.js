7ecb2e751473b6b09d64565740a761ff7f2cc8d8
kushalshit2 <kushal.shit@geotechinfo.net>
2019-03-18 01:07:48 +0530

    file write with pipe

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const fs = require('fs');
 const readfile = fs.createReadStream(__dirname + '/file.txt','utf-8')
 const wriefile = fs.createWriteStream(__dirname + '/write.txt')
 
-readfile.on('data', function(chunk){
-    console.log('new chunk recived');
-    //console.log(chunk)
-    wriefile.write(chunk);
-});
+// readfile.on('data', function(chunk){
+//     console.log('new chunk recived');
+//     //console.log(chunk)
+//     wriefile.write(chunk);
+// });
+
+readfile.pipe(wriefile)
 
