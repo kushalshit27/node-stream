78c390ffe92d9a290795c49be7b22589e8ab0d0a
kushalshit2 <kushal.shit@geotechinfo.net>
2019-03-18 01:07:07 +0530

    file write

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const http = require('http')
 const fs = require('fs');
 
+
+
 const readfile = fs.createReadStream(__dirname + '/file.txt','utf-8')
+const wriefile = fs.createWriteStream(__dirname + '/write.txt')
 
 readfile.on('data', function(chunk){
     console.log('new chunk recived');
-    console.log(chunk)
+    //console.log(chunk)
+    wriefile.write(chunk);
 });
 
